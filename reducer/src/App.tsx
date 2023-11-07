588687f3222974269b30c98007e9679a64d21f61
Adam <surzyn.adam@gmail.com>
2023-11-07 19:35:39 +0100

    Start using reducer

--- reducer/src/App.tsx
+++ reducer/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 import ShopItem from "./components/ShopItems/ShopItems";
 //TODO Koszyk ma miec:
@@ -15,6 +15,31 @@ type Item = {
   price: number;
 };
 
+enum ActionTypes {
+  ADD = "add",
+  REMOVE = "remove",
+  REMOVEALL = "removeAll",
+}
+
+type Actions = {
+  type: ActionTypes;
+  payload?: Item;
+};
+function reducer(state: Item[], action: Actions): Item[] {
+  let cartArr: Item[];
+  switch (action.type) {
+    case "add":
+      if (action.payload !== undefined) {
+        cartArr = [...state];
+        cartArr.push(action.payload);
+        return cartArr;
+      }
+      return state;
+
+    default:
+      return state;
+  }
+}
 function App() {
   const shopItems: Item[] = [
     {
@@ -36,7 +61,9 @@ function App() {
       price: 6,
     },
   ];
-
+  const [state, dispatch] = useReducer(reducer, [
+    { name: "", id: null, count: null, price: null },
+  ]);
   return (
     <div className="App">
       <div className="shop-items">
